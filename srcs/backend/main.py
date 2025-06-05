85b1002652bcd2254315cad41afc35d7180d29b3
Lukas González <86381091+lugonzalg@users.noreply.github.com>
2025-06-05 22:03:17 +0200

    Organize backend modules

---
+++ srcs/backend/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from .routes import projects
+from .data.database import Base, engine
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.on_event("startup")
+async def on_startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+app.include_router(projects.router)
